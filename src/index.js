03f0b37ed247f2490abc5e5b1ea7a44a6ad2520e
Oscar Bello <you@example.com>
2022-05-16 17:57:38 -0500

    Configuracion de React

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+// const rootElement = document.getElementById('root');
+// const root = ReactDOM.createRoot(rootElement);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );

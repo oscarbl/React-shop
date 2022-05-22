d32dfeb55aa4dfc1b0afd5002fb806b9ea6e00bb
Oscar Bello <you@example.com>
2022-05-22 00:56:58 -0500

    Challenge

--- src/components/productItem.jsx
+++ src/components/productItem.jsx
@@ -1,27 +1,24 @@
 import React from "react";
-import "../styles/ProductInfo.scss";
+import "../styles/ProductItem.scss";
 
-const ProductInfo = () => {
+const ProductItem = () => {
   return (
-    <>
+    <div className="ProductItem">
       <img
         src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        alt="bike"
+        alt=""
       />
-      <div className="ProductInfo">
-        <p>$35,00</p>
-        <p>Bike</p>
-        <p>
-          With its practical position, this bike also fulfills a decorative function,
-          add your hall or workspace.
-        </p>
-        <button className="primary-button add-to-cart-button">
-          <img src="./icons/bt_add_to_cart.svg" alt="add to cart" />
-          Add to cart
-        </button>
+      <div className="product-info">
+        <div>
+          <p>$120,00</p>
+          <p>Bike</p>
+        </div>
+        <figure>
+          <img src="./icons/bt_add_to_cart.svg" alt="" />
+        </figure>
       </div>
-    </>
+    </div>
   );
 };
 
-export default ProductInfo;
+export default ProductItem;

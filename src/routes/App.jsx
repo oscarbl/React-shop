25a87934dc442093191676d924b366ffc40651d1
Oscar Bello <you@example.com>
2022-05-22 00:48:05 -0500

    Atomic Design

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import Login from '../pages/Login';
+import PasswordRecovery from '../pages/PasswordRecovery';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route exact path='/' element={<Home/>}/>
-                    <Route exact path='/login' element={<Login/>}/>
-                    <Route exact path='/recovery-password' element={<RecoveryPassword/>}/>
-                    <Route path='*' element={<NotFound/>}/>
-                </Routes>
-            </Layout>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/password-recovery" element={<PasswordRecovery />} />
+            <Route exact path="/send-email" element={<SendEmail />} />
+            <Route exact path="/new-password" element={<NewPassword />} />
+            <Route exact path="/account" element={<MyAccount />} />
+            <Route exact path="/signup" element={<CreateAccount />} />
+            <Route exact path="/checkout" element={<Checkout />} />
+            <Route exact path="/orders" element={<Orders />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     );
 };
 
